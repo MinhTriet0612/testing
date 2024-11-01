78459a971a2983161d3909c2574638298749b7b6
Minh Triet Nguyen <90248665+MinhTriet0612@users.noreply.github.com>
2024-11-01 21:31:48 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hasdlfjkhasdkfjhadslkfjhadslfkjadhsf" << std::endl;
+  std::cout << "OKe, i'm fine" << std::endl;
   cout << "hdjakfhdsafljsdhafkl";
   return 0;
 }
